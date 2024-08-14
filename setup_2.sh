34b2cd332b1653bd820d8f57fb1861e3606f3959
xuan <lexuanhuy248@gmail.com>
2024-08-14 14:48:08 +0700

    update setup

--- setup_2.sh
+++ setup_2.sh
@@ -1,1 +1,1 @@
-echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7Cv5nyMF4/HIwE3YOS6HzM4JIMN13iTz+BEURkL6HZ" >> ~/.ssh/known_hosts
+echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" >> ~/.ssh/known_hosts
